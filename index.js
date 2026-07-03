5c68be475fb229d976a06bedf71376b4f290d637
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-03 02:57:37 -0500

    Verify webhook

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/webhook", (req, res) => {
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === "FFDigital2026") {
+    console.log("Webhook verificado correctamente");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+
+});
 app.post("/webhook", (req, res) => {
 
   console.log("Mensaje recibido");
