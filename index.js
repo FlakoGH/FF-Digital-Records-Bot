494e307febc811f834a628f99d0c68290327817c
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-03 02:21:55 -0500

    Add webhook

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("FF Digital Records Bot funcionando");
@@ -8,6 +9,15 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.post("/webhook", (req, res) => {
+
+  console.log("Mensaje recibido");
+
+  console.log(req.body);
+
+  res.sendStatus(200);
+
+});
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
