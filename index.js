16478fea8bf62609ec32ea5c44a09b3e9d7eb803
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-03 01:48:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Servidor iniciado en el puerto ${PORT}');
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
