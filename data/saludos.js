4c4aac6d0c943733c37533a1e351077c42085502
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-03 16:05:50 -0500

    Create saludos.js

--- data/saludos.js
+++ data/saludos.js
@@ -1,1 +1,23 @@
-
+const saludos = [
+  "hola",
+  "holi",
+  "hello",
+  "hi",
+  "buenas",
+  "buenos dias",
+  "buenas tardes",
+  "buenas noches",
+  "qué hubo",
+  "que hubo",
+  "q'hubo",
+  "ey",
+  "hey"
+  "Quiubo",
+  "Quiubo pues",
+  "Qué más",
+  "Todo bien",
+  "Buenas mi hermano",
+  "Buenas parcero",
+  "Hola bro"
+  ];
+module.exports = saludos;
