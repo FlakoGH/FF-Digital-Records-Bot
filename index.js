8939031c0551f2f24d6765826cd5c4dc926dbe7a
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-04 23:19:22 -0500

    Add logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const saludos = require("./data/saludos");
+const logger = require("./logger");
 
 const app = express();
 app.use(express.json());
