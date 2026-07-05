02e603df54ae1e4c885c8c3ce4f797dea1b800a4
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-05 17:48:35 -0500

    Edit variable function

--- whatsapp/enviarTexto.js
+++ whatsapp/enviarTexto.js
@@ -2,39 +2,39 @@ const axios = require("axios");
 
 async function enviarTexto(numero, mensaje) {
 
-    await axios.post(
+    try {
 
-        `https://graph.facebook.com/v23.0/${process.env.PHONE_NUMBER_ID}/messages`,
+        await axios.post(
 
-        {
+            `https://graph.facebook.com/v25.0/${process.env.PHONE_NUMBER_ID}/messages`,
 
-            messaging_product: "whatsapp",
+            {
+                messaging_product: "whatsapp",
+                to: numero,
+                type: "text",
+                text: {
+                    body: mensaje
+                }
+            },
 
-            to: numero,
-
-            type: "text",
-
-            text: {
-
-                body: mensaje
-
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.TOKEN}`,
+                    "Content-Type": "application/json"
+                }
             }
 
-        },
+        );
 
-        {
+    } catch (error) {
 
-            headers: {
+        console.error("❌ Error enviando mensaje:");
 
-                Authorization: `Bearer ${process.env.TOKEN}`,
+        console.error(error.response?.data || error.message);
 
-                "Content-Type": "application/json"
+        throw error;
 
-            }
-
-        }
-
-    );
+    }
 
 }
 
