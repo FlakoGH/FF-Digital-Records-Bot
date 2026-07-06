e6181852ec1dd1ac24f3eb4f9ec67aed46cd0c88
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-05 20:39:46 -0500

    Crear menuMusica.js

---
+++ menus/menuMusica.js
@@ -0,0 +1,15 @@
+const menuMusica = `
+🎵 *MENÚ MÚSICA*
+
+1️⃣ Spotify
+2️⃣ YouTube
+3️⃣ Deezer
+4️⃣ Apple Music
+5️⃣ Amazon Music
+6️⃣ Todas las plataformas
+
+⬅️ 0. Volver
+🏠 99. Inicio
+`;
+
+module.exports = menuMusica;
