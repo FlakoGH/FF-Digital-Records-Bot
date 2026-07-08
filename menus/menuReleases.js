757ba7a49cd6d2413aa6c539f33f4d8f4e8069c0
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-08 13:14:05 -0500

    Crear menuReleases.js

---
+++ menus/menuReleases.js
@@ -0,0 +1,13 @@
+const menuReleases = `🎵 *MENÚ RELEASES*
+
+1️⃣ Último lanzamiento
+
+2️⃣ Catálogo completo
+
+3️⃣ Próximos lanzamientos
+
+⬅️ 0. Volver
+
+🏠 99. Inicio`;
+
+module.exports = menuReleases;
