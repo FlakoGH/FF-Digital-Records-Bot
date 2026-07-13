cf9159b938f007b16dc2afd7d01434b4f4654099
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-13 17:35:10 -0500

    Create links y musica.js

---
+++ links/musica.js
@@ -0,0 +1,15 @@
+const spotify = "https://open.spotify.com/artist/...";
+const youtube = "https://youtube.com/@...";
+const deezer = "https://www.deezer.com/artist/...";
+const appleMusic = "https://music.apple.com/...";
+const amazonMusic = "https://music.amazon.com/...";
+const todas = "https://beacons.ai/djflakoflow";
+
+module.exports = {
+  spotify,
+  youtube,
+  deezer,
+  appleMusic,
+  amazonMusic,
+  todas
+};
