18aef8bd015a7e653f2db4638fc77ad01aa456d9
Erik Sevilla <109993580+FlakoGH@users.noreply.github.com>
2026-07-26 18:24:31 -0500

    Update menuContacto.js

--- menus/menuContacto.js
+++ menus/menuContacto.js
@@ -1,13 +1,14 @@
-const menuContacto = `📞 *MENÚ CONTACTO*
+const menuContacto = `
+📞 *MENÚ CONTACTO*
 
-1️⃣ WhatsApp
-
-2️⃣ Correo
-
-3️⃣ Telegram
+1️⃣ Hablar con lider 👤
+2️⃣ WhatsApp personal 💬
+3️⃣ Correo del sello 📧
+4️⃣ Correo artístico 🎵
+5️⃣ Solicitar distribución 🚀
 
 ⬅️ 0. Volver
-
-🏠 99. Inicio`;
+🏠 99. Inicio
+`;
 
 module.exports = menuContacto;
